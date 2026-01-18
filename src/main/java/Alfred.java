fcb31a44f293cd6f7d130d40cb787866a615d29d
Luke Tan <106603513+bingmybongg@users.noreply.github.com>
2026-01-18 22:33:03 +0800

    Renamed my chatbot to Alfred and made a skeleton greeting

---
+++ src/main/java/Alfred.java
@@ -0,0 +1,10 @@
+public class Alfred {
+    public static void main(String[] args) {
+        String initialGreeting = "Good morning Master!\nWhat do you need from me?\n";
+        String endChat = "Good day Sir!\n";
+
+        System.out.println(initialGreeting);
+        System.out.println(endChat);
+
+    }
+}
