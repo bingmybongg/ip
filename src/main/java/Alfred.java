40128c0cfd2ef9ab8b0c65d917ca964858c85f6f
Luke Tan <106603513+bingmybongg@users.noreply.github.com>
2026-01-18 22:44:56 +0800

    Added an echo function for Alfred. As well for it to end whenever any form of 'bye' is said

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Alfred {
     public static void main(String[] args) {
         String initialGreeting = "Good morning Master!\nWhat do you need from me?\n";
         String endChat = "Good day Sir!\n";
 
         System.out.println(initialGreeting);
+
+        while (true) {
+            Scanner reader = new Scanner(System.in);
+            String readInput = reader.nextLine();
+            String separator = "What ese do you need?\n";
+
+            if (readInput.toLowerCase().contains("bye")) {
+                reader.close();
+                break;
+            }
+            System.out.println("\n" + readInput + "\n" + separator);
+        }
         System.out.println(endChat);
 
     }
